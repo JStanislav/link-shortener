85613e4627c20c1c37e2d09daaaa62699d0c869f
Joaquín <jstaneck@outlook.com>
2020-12-09 18:19:36 -0300

    minimal funcionality

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,22 @@
 from app.main import bp
+from flask import render_template, jsonify
+from app.main.forms import ShortForm
+import random
 
-@bp.route('/')
+@bp.route('/generate_url/<string:url>')
+def generate_url(url):
+    result = ""
+    length = 5
+    for _ in range(length):
+        result += chr(random.randint(65, 90))
+    print(result)
+    return result
+    #return jsonify({'result': result})
+
+
+@bp.route('/', methods=['GET', 'POST'])
 def index():
-    return "Hello world"
+    form = ShortForm()
+    if form.validate_on_submit():
+        return render_template("index.html", form=form, generated=generate_url(form.input.label))
+    return render_template("index.html", form=form)
